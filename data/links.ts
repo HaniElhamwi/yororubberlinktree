27786373cb67aac1ae0a10efe584bc9e74692e6c
HaniElhamwi <hanialhamwi000000@gmail.com>
2023-08-29 16:13:30 +0300

    first commit

---
+++ data/links.ts
@@ -0,0 +1,49 @@
+const socialLinks = [
+  "https://www.linkedin.com/company/sial-co/",
+  "https://www.instagram.com/sialtr/?igshid=MjEwN2IyYWYwYw%3D%3D",
+  "https://www.facebook.com/people/Sial/100093416567903/?mibextid=LQQJ4d",
+  "https://www.google.com/maps/search/?api=1&query=%C3%87%C4%B0LEK+MAH.,+63147+SK.,+NO:+1,+%C4%B0%C3%87+KAPI+NO:+8,+MEGACENTER+I%C5%9E+MERKEZI,+Akdeniz/Mersin",
+];
+const profileData = {
+  userName: "Sial",
+  // photoLink: logo,
+  desc: "foodstuffs | Oils",
+};
+
+const linkData = [
+  {
+    id: 1,
+    link: "tel:+903245042424",
+    name: "Tel  ",
+    image: "tel:+903245042424",
+  },
+  {
+    id: 2,
+    link: "tel:+905013730633",
+    name: "Warehouse Gsm",
+    image: "tel:+905013730633",
+  },
+  {
+    id: 3,
+    link: "tel:+905011210633",
+    name: "Logistics Gsm",
+    image:
+      "https://play-lh.googleusercontent.com/QYc9QNf0_c-M7rhF4eo8daWbIRg_ukwzabR3Xj9TX_6zxG-AbzhKF5NS3CAd2X79fm0=s360-rw",
+  },
+  {
+    id: 4,
+    link: "mailto:info@sialtr.com",
+    name: "Email Address",
+    image:
+      "https://play-lh.googleusercontent.com/QYc9QNf0_c-M7rhF4eo8daWbIRg_ukwzabR3Xj9TX_6zxG-AbzhKF5NS3CAd2X79fm0=s360-rw",
+  },
+  {
+    id: 5,
+    link: "http://www.sialtr.com/",
+    name: "Website",
+    image:
+      "https://play-lh.googleusercontent.com/QYc9QNf0_c-M7rhF4eo8daWbIRg_ukwzabR3Xj9TX_6zxG-AbzhKF5NS3CAd2X79fm0=s360-rw",
+  },
+];
+
+export { socialLinks, profileData, linkData };
